393bd9095b827daf4f09d9c8f6cc8b3db0145bdd
Eranga86 <chaminieranga70@gmail.com>
2025-09-28 13:16:24 +0530

    Update main.py with fixes

--- main.py
+++ main.py
@@ -56,4 +56,6 @@ async def predict(file: UploadFile = File(...)):
         )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=3000)
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
+    port = int(os.environ.get("PORT", 3000))  # default to 3000 for local testing
